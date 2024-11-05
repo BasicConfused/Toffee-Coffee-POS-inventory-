17ec7d38d244ece2859dc4d6276f028372b31fae
Bandar Ansary <ansarybandar@gmail.com>
2024-11-05 09:07:41 +0800

    added a html file named index.html

---
+++ src/main/java/com/doubleG/toffeeCoffee/ToffeeCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.doubleG.toffeeCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToffeeCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToffeeCoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doubleG/toffeeCoffee/ToffeeCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doubleG.toffeeCoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToffeeCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
