9acfe19554405d8a6e8502bfd93e91658e042e30
Bandar Ansary <ansarybandar@gmail.com>
2024-11-07 19:34:46 +0800

    added a tailwind framework as a frontend

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./toffeeCoffee/src/main/resources/static/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
