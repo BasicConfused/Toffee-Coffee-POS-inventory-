d497785177461178ae74f42044879700b38ee968
Bandar Ansary <ansarybandar@gmail.com>
2024-11-12 21:35:44 +0800

    new error found, add new design at dashboard

--- src/main/java/com/doubleG/toffeeCoffee/ToffeeCoffeeApplication.java
+++ src/main/java/com/doubleG/toffeeCoffee/ToffeeCoffeeApplication.java
@@ -1,7 +1,10 @@
 package com.doubleG.toffeeCoffee;
 
+import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class ToffeeCoffeeApplication {
@@ -9,5 +12,4 @@ public class ToffeeCoffeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToffeeCoffeeApplication.class, args);
 	}
-
 }
--- src/main/java/com/doubleG/toffeeCoffee/indexController.java
+++ src/main/java/com/doubleG/toffeeCoffee/indexController.java
@@ -1,4 +1,20 @@
 package com.doubleG.toffeeCoffee;
 
-public class indexControler {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class indexController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/register")
+    public String userRegistration(){
+        return "index";
+    }
 }
